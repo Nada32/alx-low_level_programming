4a4d81b855e81a6a3cee8034eba650f199582f38
Nada <dodynn29@gmail.com>
2023-08-11 15:57:41 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -9,22 +10,26 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        char dody[]=n
+	char int_string[20];
+
+	itoa(var, int_string, 10);
+        char dody[100]=int_string;
         int i;
-        char x=0;	
+        char x;
         for(i=0;i<sizeof(dody);i++)
 	{
 	x=dody[i];
         }
-	if(x>5)
+	xx=(int)x;
+	if(xx>5)
 	{
 		printf("Last digit of %c is %c and is greater than 5",x);
 	}
-	else if(x==0)
+	else if(xx==0)
         {
                 printf("Last digit of %c is %c and is 0",x);
         }
-	else if(x<6 && x!=0)
+	else if(xx<6 && x!=0)
         {
                 printf("Last digit of %c is %c and  is less than 6 and not 0",x);
         }
