5e94a7eafc31a503e886059e97b8eb321c21c4fc
Nada <dodynn29@gmail.com>
2023-08-11 16:20:39 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,33 +7,33 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        char dody=(char)(n);
+        int n;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        char dody[20];
+        sprintf(dody,"%d",n);
         int i;
         char x;
         for(i=0;i<sizeof(dody);i++)
-	{
-	x=dody[i];
+        {
+        x=dody[i];
         }
-	xx=(int)x;
-	if(xx>5)
-	{
-		printf("Last digit of %c is %c and is greater than 5",x);
-	}
-	else if(xx==0)
+        int xx=(int)x;
+        if(xx>5)
+        {
+                printf("Last digit of %c is %c and is greater than 5",x);
+        }
+        else if(xx==0)
         {
                 printf("Last digit of %c is %c and is 0",x);
         }
-	else if(xx<6 && x!=0)
+        else if(xx<6 && x!=0)
         {
                 printf("Last digit of %c is %c and  is less than 6 and not 0",x);
         }
-	else
+        else
         {
                 printf("try");
         }
-	/* your code goes there */
-	return (0);
+          return (0);
 }
