9abef22c6dc22dbc15867b97d4e7b8f4fac9284a
Nada <dodynn29@gmail.com>
2023-08-11 17:22:20 -0700

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+        int i;
+	char a='a';
+	char z='z';
+	for (i=(int)(a);i<(int)(z);i++)
+		putchar((char)(i));
+
+	return (0);
+
+}
