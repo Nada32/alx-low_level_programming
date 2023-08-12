e6e432b64107348a8a4f4445fe2a36fa201ade41
Nada <dodynn29@gmail.com>
2023-08-12 13:03:46 -0700

    d

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all single digit numbers of base 10 .
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 0 ; i < 16 ; i++)
+	{
+	x = i;
+	printf("%d", x);
+	}
+	putchar('\n');
+
+	return (0);
+}
