597ef8757eb79e755478a2289afee44f153391ff
Nada <dodynn29@gmail.com>
2023-08-13 12:03:49 -0700

    d

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all single digit numbers of base 10 .
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int x;
+	int y;
+	int z;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+	for (x = i ; x < 10 ; x++)
+	{
+	for (y = 0 ; y < 10 ; y++)
+        {
+        for (z = y + 1 ; z < 10 ; z++)
+        {
+	putchar(i + '0');
+        putchar(x + '0');
+	putchar(' ');
+        putchar(y + '0');
+        putchar(z + '0');
+	if (i < 8 || x < 8 || y < 8 || z < 8)
+	putchar(',');}}}}
+
+	putchar('\n');
+
+	return (0);
+}
