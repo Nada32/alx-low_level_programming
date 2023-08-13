50727ceb424ed00f42df8cf04c37c27fb7d69c7b
Nada <dodynn29@gmail.com>
2023-08-13 10:01:15 -0700

    d

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all single digit numbers of base 10 .
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+	for (x = i ; x < 10 ; x++)
+	{
+	putchar(i + '0');
+	putchar(x + '0');
+	
+	if (i < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}}
+	}
+	putchar('\n');
+
+	return (0);
+}
