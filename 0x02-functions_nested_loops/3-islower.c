68b488b1f96b72130583129f970d288220f44912
Nada <dodynn29@gmail.com>
2023-08-16 19:44:51 -0700

    d

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - checks the lower  alphabet, in lowercase .
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	char a = 'a';
+	char z = 'z';
+	int i = (int)(a);
+	int j = (int)(z);
+
+	if (a < c < z)
+	return 1;
+
+	else
+	return 0;
+
+
+}
