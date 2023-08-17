55add95cd852e47a58661cbb90f629cc48151a24
Nada <dodynn29@gmail.com>
2023-08-17 10:47:58 -0700

    d

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit .
+ * @z: parameter
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int z)
+{
+	int zz;
+
+	zz = z%10;
+
+	return (zz);
+}
