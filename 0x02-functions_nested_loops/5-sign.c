c5903c26adb839d5c79406b176ba258133c6be82
Nada <dodynn29@gmail.com>
2023-08-17 06:26:01 -0700

    d

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  print_sign - prints the sign of a num .
+ * @n: parameter any char
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	char a = 'A';
+	char z = 'z';
+	int i = (int)(a);
+	int j = (int)(z);
+
+	if (c > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (c < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else if (c == 0)
+	{
+	_putchar('0');
+	return (0);
+	
+	}
+
+}
