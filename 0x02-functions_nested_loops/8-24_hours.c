6edb7d22c84d1dc4dd66d3081ec040ef3ec2c713
Nada <dodynn29@gmail.com>
2023-08-17 10:35:51 -0700

    d

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,21 +11,28 @@ void jack_bauer(void)
 	int j;
 	int k;
 	int l;
+	int x;
 
 	for (i = 0 ; i < 3 ; i++)
 {
-	for (j = 0 ; j < 4 ; j++)
-{	
+	if (i < 2)
+	x = 10;
+	else if (i == 2)
+	x = 4;
+	for (j = 0 ; j < x ; j++)
+{
 	for (k = 0 ; k < 6 ; k++)
 {
 	for (l = 0 ; l < 10 ; l++)
-{	
+{
+	
 	_putchar(i + '0');
 	_putchar(j + '0');
 	_putchar(':');
 	_putchar(k + '0');
 	_putchar(l + '0');
 	_putchar('\n');
+
 }}}}
 
 }
