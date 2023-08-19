1c3b29e6e70cde6fda8d041aa43456264e26a056
Nada <dodynn29@gmail.com>
2023-08-19 12:29:07 -0700

    d

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: parameter
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+	for (j = 0 ; j < n ; j++)
+	{
+	for (i = 0 ; i < n ; i++)
+	_putchar('#');
+	_putchar('\n');
+	}}
+	else
+	_putchar('\n');
+}
