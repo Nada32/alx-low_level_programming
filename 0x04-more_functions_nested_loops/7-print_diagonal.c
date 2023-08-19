93a01cddb84bb29c02e834e2ac9ff214096b9b3e
Nada <dodynn29@gmail.com>
2023-08-18 19:57:38 -0700

    d

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_diagonal - prints diagonal
+ * @n: parameter
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+        int i;
+        int j;
+	if (n > 0)
+	{
+        for (j = 0 ; j < n ; j++)
+        {
+        for (i = 0 ; i < j ; i++)
+        _putchar('\\');
+        _putchar('\n');
+        }}
+	else
+	_putchar('\n');
+        return (0);
+}
