62541622182b35d4f72a5f9d0b70376989ec71a2
Nada <dodynn29@gmail.com>
2023-08-18 18:58:26 -0700

    d

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,16 +13,17 @@ void more_numbers(void)
 	int k;
 	char dd;
 
-	for (j = 0; j < 10 ; j++)
+	for (j = 0 ; j < 10 ; j++)
 {
 	for (k = 0 ; k < 2 ; k++)
 	for (i = 0 ; i < 10 ; i++)
 {
-	if ((i < 9 && k == 0)|| (i < 5 && k == 1))
+	if ((i < 9 && k == 0) || (i < 5 && k == 1))
 	{
-	if (k == 1)
+
 	dd = (char)(k);
 	d = (char)(i);
+	if (k == 1)
 	_putchar(dd + '0');
 	_putchar(d + '0');
 }
