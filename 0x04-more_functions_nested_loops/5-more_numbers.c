81c30ac0c55df09ffeaf2219b70d0a5c79d2bf93
Nada <dodynn29@gmail.com>
2023-08-18 18:44:01 -0700

    d

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints nums x10 .
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char d;
+	int j;
+	int k;
+	char dd;
+
+	for (j = 0; j < 10 ; j++)
+{
+	for (k = 0 ; k < 2 ; k++)
+	for (i = 0 ; i < 10 ; i++)
+{
+	if (i < 5 && k == 1)
+	{
+	dd = (char)(k);
+	d = (char)(i);
+	_putchar(dd + '0');
+	_putchar(d + '0');
+}
+	else if (k == 0)
+{
+	d = (char)(i);
+	_putchar(d + '0');
+
+}}	
+	_putchar('\n');
+
+}}
