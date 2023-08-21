6533367ccc0dc2a1a2aefd518c0923e24311accb
Nada <dodynn29@gmail.com>
2023-08-21 14:46:55 -0700

    d

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swapping
+ * @a: parameter
+ * @b: parameter
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*b = temp;
+
+
+}
