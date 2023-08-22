4ac02863821f4913625f87a8fd5ab20a7c9ea433
Nada <dodynn29@gmail.com>
2023-08-21 21:09:05 -0700

    d

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,9 @@ void puts_half(char *str)
 
 	mn = len;
 	i = len / 2;
-	if(str[len / 2] == ' ')
+	if (str[len / 2] == ' ')
 	{
-	i = (len / 2) - 1;
+	i = (len / 2) + 1;
 	len = mn;
 	}
 	for (; i < len ; i++)
