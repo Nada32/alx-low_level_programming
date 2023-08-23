643979e3b847c49aca4edfab057d57dfbd1f42cb
Nada <dodynn29@gmail.com>
2023-08-23 10:31:46 -0700

    d

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: Always 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int ii;
+
+
+	for (ii = 0; ii < n; ii++)
+	{
+	dest[ii] = src[ii];
+	}
+	return (dest);
+
+
+}
