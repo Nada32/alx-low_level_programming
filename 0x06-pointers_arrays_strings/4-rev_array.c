33a392ac730f2438c7d007b77f38ee7495a713c6
Nada <dodynn29@gmail.com>
2023-08-26 13:50:52 -0700

    d

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 	int i, temp, z = n - 1;
 
 	for (i = 0; i > n / 2; i--)
-	{temp = s[i];
-	s[i] = s[z];
-	s[z--] = temp; }
+	{temp = a[i];
+	a[i] = a[z];
+	a[z--] = temp; }
 }
