0c18e8a494ab15ddcdd2cbd7a4b7cfca5bc163fb
Nada <dodynn29@gmail.com>
2023-08-26 12:29:46 -0700

    d

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -23,8 +23,7 @@ int _strcmp(char *s1, char *s2)
 	m = len1;
 	else if (len1 <= len2)
 	m = len2;
-	for (i = 0; i < m; i++)
-	{
+	for (i = 0; i < m; i++) {
 	if (s1[i] == s2[i])
 	z++;
 	else if (s1[i] > s2[i])
@@ -33,20 +32,15 @@ int _strcmp(char *s1, char *s2)
 	{x = (int)(s2[i]) + x; }}
 	if ((z == len1) && (z  == len2))
 	k = 0;
-	else if (y > x)
-	{
-	for (i = 0; i < m; i++)
-	{
+	else if (y > x) {
+	for (i = 0; i < m; i++) {
 	if (s1[i] != s2[i])
 	{b = (int)(s1[i]) - (int)(s2[i]);
 	break; }}
 	k = b; }
-	else if (x > y)
-	{
-	for (i = 0; i < m; i++)
-	{
-	if (s1[i] != s2[i])
-	{n = (int)(s1[i]) - (int)(s2[i]);
+	else if (x > y) {
+	for (i = 0; i < m; i++) {
+	if (s1[i] != s2[i]) {n = (int)(s1[i]) - (int)(s2[i]);
 	break; }}
 	k = n; }
 	return (k); }
