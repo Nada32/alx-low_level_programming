ece4f705d2a23ad6afe87b85bf8f9b67a7b6343d
Nada <dodynn29@gmail.com>
2023-08-26 12:06:34 -0700

    d

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,11 +16,9 @@ int _strcmp(char *s1, char *s2)
 	int x = 0;
 
 	while (s1[len1] != '\0')
-	{
-	len1++; }
+	{len1++; }
 	while (s2[len2] != '\0')
-	{
-	len2++; }
+	{len2++; }
 	if (len1 > len2)
 	m = len1;
 	else if (len1 <= len2)
