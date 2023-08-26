bb14fb899cd87ea92c9ac062bac4ee51e7feff2d
Nada <dodynn29@gmail.com>
2023-08-26 14:21:35 -0700

    d

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - all upper case
+ * @c: parameter
+ * Return: Always 0 (Success)
+ */
+char *string_toupper(char *c)
+{
+	int i, len = 0;
+
+	while (c[len] != '\0')
+	len++;
+	for (i = 0; i <= len; i++)
+	if ((c[i] >= 'a') && (c[i] < ('z' + 1)))
+	{
+	c[i] = c[i] - 32;
+	}
+	
+}
