cb5898a38e84bc8ce9b64dcd24337e84cec4ddad
Nada <dodynn29@gmail.com>
2023-08-26 17:34:27 -0700

    d

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,14 @@ char *leet(char *c)
 	int i, j, len = 0;
 	int arr[10] = {4,4,3,3,0,0,7,7,1,1};
 	char s[] = "aAeEoOtTlL";
-	
-	while (c[len] != '\0')
-	len++;
-	for (i = 0; i < len; i++)
+
+	for (i = 0; c[i] != '\0'; i++)
 	{
 	for (j = 0; j < 10; j++)
-	(c[i] == s[j]) ? (c[i] = '0' + arr[j]) : (c[i] = c[i]);
+	{
+	if (c[i] == s[j])
+	c[i] = '0' + arr[j];}
+	i++;
 	}
 	return (c);
 }
