0f47861bf03cacc597d39651102a13fb9f5f07ee
Nada32 <dodynn29@gmail.com>
2023-08-29 11:27:25 -0700

    d

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memcpy - filling memo with a const byet
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: Always 0 (Success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}
