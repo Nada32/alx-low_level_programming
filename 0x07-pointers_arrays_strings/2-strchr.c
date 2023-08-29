f404cee0411dd8f4640e3fae47c13d2187e16bec
Nada32 <dodynn29@gmail.com>
2023-08-29 15:38:55 -0700

    d

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * *_strchr - filling memo with a const byet
+ * @s: parameter
+ * @c: parameter
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i, j, z = 0, len = 0;
+	while (s[len] != '\0')
+	len++;
+
+	char ss[len];
+	for (i = 0; i < len; i++)
+	{
+	if (s[i] == c)
+	{
+	for (j = i; j < len; j++)
+	{
+	ss[j] = s[i + z];
+	return (ss[j]);
+	z++;
+	}
+	break;
+	}
+	else 
+	{ss[i] = NULL;
+	return (ss[i]); }
+	}
+	
+}
