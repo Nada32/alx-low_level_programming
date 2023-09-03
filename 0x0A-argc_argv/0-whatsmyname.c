c1a758154cf9774001c242dc9743054a702fc23f
Nada32 <dodynn29@gmail.com>
2023-09-03 16:13:17 -0700

    d

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -8,6 +9,6 @@
  */
 int main(int argc, char *argv[])
 {
-	itsname(argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
