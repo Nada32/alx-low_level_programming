b5a4d3e8ccb749232ed36a823408fdd09bf64b9e
Nada32 <dodynn29@gmail.com>
2023-09-04 03:19:11 -0700

    d

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: parameter
+ * @argv: parameter
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
