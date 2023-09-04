813c1936fb39aea0a10e8fecb6dde28fddf21ccb
Nada32 <dodynn29@gmail.com>
2023-09-04 05:12:25 -0700

    d

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,26 +4,13 @@
 
 /**
  * main - Entry point
- * itsname - print name
  * @argc: parameter
  * @argv: parameter
- * @c: parameter
  * Return: Always 0.
  */
-void itsname(char *c);
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	itsname(argv[0]);
 	return (0);
-}
-void itsname(char *c)
-{
-	int i;
-
-	for (i = 0; c[i] != '\0'; i++)
-	{
-	_putchar(c[i]);
-	}
-	_putchar('\n');
 }
