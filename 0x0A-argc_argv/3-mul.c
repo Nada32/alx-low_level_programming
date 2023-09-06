92e637d59201fcf5f9c19b4486febdbf1365af0d
Nada32 <dodynn29@gmail.com>
2023-09-06 16:26:01 -0700

    d

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,28 @@
  * @argv: parameter
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
-{
+int main(int argc, char **argv)
+	{
+
+	 int i, z, x = 0, y = 0;
 
 	if (argc < 2)
 	{
 	printf("Error\n");
 	return (1);
 	}
+	for (i = 0; *argv[1] != '\0'; i++)
+	{
+	x = x * 10 + (*argv[1] - 48);
+	}
+	for (i = 0; *argv[2] != '\0'; i++)
+        {
+        y = y * 10 + (*argv[2] - 48);
+        }
+	z = x * y;
 
-	printf("%d\n", ((int) *argv[1]) * ((int) *argv[2]));
+	printf("%d\n", z);
 
 	return (0);
-}
+
+	}
