b3d476a8a14c67f1bd0fd36588ce8d640d3a8824
Nada32 <dodynn29@gmail.com>
2023-09-06 13:35:15 -0700

    d

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,21 +10,17 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, z = 0;
+	char *p;
+	p = s1;
+	p = s2;
+
 	int sizee = sizeof(s1) + sizeof(s2);
 	char *arr = (char *) malloc(sizee + 1);
+	p = arr;
 
-	if (arr == NULL || s1 == NULL || s2 == NULL)
-	return (NULL);
-	while(s1[i] != '\0')
-	{arr[i] = s1[i];
-	i++;
-	}
-	for(i = sizee / 2; (i < sizee) && (s2[z] != '\0'); i++)
-	{
-	arr[i] = s2[z];
-	z++;
-	}
+	while (*s1)
+	*p++ = *s1++;
+	while (*s2)
+	*p++ = *s2++;
 	return (arr);
-
 }
