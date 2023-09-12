de2eaa4457ca5d99d5d883efcaf5d606d7e858cc
Nada32 <dodynn29@gmail.com>
2023-09-12 15:22:44 -0700

    d

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structuer
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Descriptio: It takes three parameters
+ */
+struct dog
+{
+char name;
+float age;
+char *owner;
+
+
+
+};
+
+
+
+#endif
