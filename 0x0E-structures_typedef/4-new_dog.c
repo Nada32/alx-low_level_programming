136df1f6da41fc078c0f69b27789cb6087e9de58
Nada32 <dodynn29@gmail.com>
2023-09-13 07:53:53 -0700

    d

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - new initialize a variable of type struct
+ * @d: parameter
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d == NULL)
+	;
+	else
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
