75cc5b7cb077b0239b8c5516dd88c94c1c7b086c
Nada32 <dodynn29@gmail.com>
2023-09-13 18:55:05 -0700

    d

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print array
+ * @array: parameter
+ * @size: parameter
+ * @action: parameter
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
 #ifndef FUN_P
 #define FUN_P
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 #endif
