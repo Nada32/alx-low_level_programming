5d6a8c6189a9704bb77b6ec901a2f3b8e1494005
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-21 00:43:09 -0700

    d

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,10 +13,12 @@ int i;
 op_t ops[] = {{"+", op_add}, {"-", op_sub},
 {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
 
-for (i = 0; i < 5; i++)
+i = 0;
+while (i < 5)
 {
 if (s == ops[i].op)
 return (ops[i].f);
+i++;
 }
 return (NULL);
 
