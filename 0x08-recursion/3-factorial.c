84a684b30c2df78b51e5aca6fa75975c3cfd84d0
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-21 09:16:00 -0700

    d

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * @n: parameter
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+
+if (i <= 1)
+return 1;
+return (i * factorial(i - 1));
+	
+}
