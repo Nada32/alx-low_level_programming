525d4e02ad96be8fb11efa56557b717226b3fe2a
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-21 18:20:38 -0700

    d

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function
+ * @h: parameter
+ *
+ * Description: prints all elements
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+const list_t *cur = h;
+int i = 0;
+while (h != NULL)
+{
+i++;
+cur = cur->next;
+}
+return (i);
+
+}
