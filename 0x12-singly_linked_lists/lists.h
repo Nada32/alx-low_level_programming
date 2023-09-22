1fcf3a87007815cf9ebc59cf91bd2bad10be6196
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-21 17:55:27 -0700

    d

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,14 @@
 #ifndef LIST_H
 #define LIST_H
 #include <stdio.h>
-typedef struct size
+
+typedef struct list_s
 {
 char *str;
 unsigned int len;
 struct list_s *next;
-} size_t;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
