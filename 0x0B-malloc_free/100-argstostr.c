ac1f89b1e0736ea5876da6912120718a20850d2a
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-22 09:59:33 -0700

    d

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - array
+ * @av: parameter
+ * @ac: parameter
+ * Return: Always 0.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *arr;
+
+	arr = NULL;
+	if ((ac == 0) || (av == NULL))
+	return (NULL);
+	arr = (char *) malloc(ac * sizeof(char));
+	if ((arr == NULL))
+	{
+	return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	arr[i] = c;
+	return (arr);
+}
