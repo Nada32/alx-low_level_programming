da5cedca7c6ac6e10024b498401928c2f05e543f
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-22 10:28:17 -0700

    d

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,8 +23,9 @@ char *argstostr(int ac, char **av)
 	}
 	for (i = 0; i < ac; i++)
 	{
-	for (j = 0; *av[j] != '\0'; j++)
-	arr[i][j] = *av[i][j];
+	for (j = 0; av[i][j] != '\0'; j++)
+	arr[i] = av[i][j];
+	arr[i] = '\n';
 	}
 	return (arr);
 }
