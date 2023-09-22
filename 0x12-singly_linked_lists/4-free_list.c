36f6a8c5d04772a46ec2a2d565bbfc62c876ca0c
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-21 19:45:04 -0700

    d

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - function
+ * @head: parameter
+ *
+ * Description: prints all elements
+ * Return: number of nodes
+ */
+void free_list(list_t *head)
+{
+list_t *X;
+
+X = head;
+head = head->next;
+free(x);
+}
+
