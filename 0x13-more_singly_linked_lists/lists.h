87518ab97e8c451466188ba8c85c44e830dd92cd
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-26 13:53:44 -0700

    D

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,21 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
+/**
+ * struct list_s - short description
+ * @str: string - parameter
+ * @len: parameter
+ * @next: points to the next node
+ *
+ * Description: long descrition
+ */
+typedef struct listint_s
+{
+char *str;
+unsigned int len;
+struct listint_s *next;
+} listint_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
