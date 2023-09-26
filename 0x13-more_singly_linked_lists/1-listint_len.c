97bc7c150d8f3677d6e9b4ba6353b171e5ba40be
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-26 15:14:10 -0700

    d

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,19 +2,18 @@
 #include "lists.h"
 
 /**
- * print_listint - function
+ * listint_len - function
  * @h: parameter
  *
  * Description: prints all elements
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 size_t i = 0;
 
 while (h != NULL)
 {
-printf("%d\n", h->n);
 h = h->next;
 i++;
 }
