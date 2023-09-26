7e110614e698db7f8c8999d71db16079168ebdc3
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-26 16:38:58 -0700

    d

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function
+ * @head: parameter
+ *
+ * Description: prints all elements
+ * Return: number of nodes
+ */
+void free_listint2(listint_t *head)
+{
+listint_t *em;
+if (head == NULL)
+{
+return;
+}
+while (*head)
+{
+em = *head->next;
+free(*head);
+*head = em;
+}
+*head = NULL;
+}
+
