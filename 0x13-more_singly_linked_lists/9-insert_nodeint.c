5dd3bb460bf9cb993c650ca2fa076bf07462010f
Nada32 <52667595+Nada32@users.noreply.github.com>
2023-09-27 04:39:30 +0300

    d

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,20 +2,43 @@
 #include "lists.h"
 
 /**
- * sum_listint - function
+ * insert_nodeint_at_index - function
  * @head: parameter
+ * @idx: parameter
+ * @n: parameter
  *
  * Description: prints all elements
  * Return: number of nodes
  */
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+listint_t *ne, *k;
 unsigned int i = 0;
 
-while (head != NULL)
+if (head == NULL)
+return (NULL);
+ne = malloc(sizeof(listint_t));
+if (ne == NULL)
+return (NULL);
+ne->n = n;
+if (idx == 0)
 {
-i += head->n;
-head = head->next;
+ne->next = *head;
+*head = ne;
+return (ne);
 }
-return (i);
+k = *head;
+while (i < idx - 1)
+{
+if (k == NULL)
+{
+free(ne);
+return (NULL);
+}
+k = k->next;
+i++;
+}
+ne->next = k->next;
+k->next = ne;
+return (ne);
 }
