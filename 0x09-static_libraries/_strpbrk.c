8c433fe011420877fba485e98d9ffd7df87e2d81
Nada32 <52667595+Nada32@users.noreply.github.com>
2023-09-27 18:51:45 +0300

    d

--- 0x09-static_libraries/_strpbrk.c
+++ 0x09-static_libraries/_strpbrk.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts - write string
+ * _na - write string
  * @str: parameter
  * Return: Always 0 (Success)
  */
-void _puts(char *str)
+void _na(char *str)
 {
 	int i;
 
