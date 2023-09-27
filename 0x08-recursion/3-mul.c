f4a03dde4f11adac6fb1d9c565ef8ac6be9a8c5e
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-27 11:18:35 -0700

    d

--- 0x08-recursion/3-mul.c
+++ 0x08-recursion/3-mul.c
@@ -10,14 +10,19 @@
  */
 int main(int argc, char **argv)
 {
-int mult = 0;
+int i, j;
 
-if (argc != 3)
+if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 return (1);
 }
-mult = atoi(argv[1]) * atoi(argv[2]);
-printf("%i\n", mult);
+else
+{
+j = 1;
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+printf("%d\n", j);
+}
 return (0);
 }
