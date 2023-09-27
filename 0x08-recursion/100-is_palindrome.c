fb57953838b0550a16e795a61e304eedadfe0936
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-27 10:28:05 -0700

    d

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,44 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * is_palindrome - function
- * @s:parameter
- * Return: Always 0 (Success)
+ * _strlen_recursion - leng.
+ * @s: parameter
+ * Return: nothing
+ */
+int _strlen_recursion(char *s)
+{
+if (*s)
+	{
+s++;
+return (1 + _strlen_recursion(s));
+	}
+return (0);
+}
+/**
+ * c - extra
+ * @str: the string
+ * @len: parameter
+ * @count: counter of recursion
+ * Return: 1 if string is a palindrome, 0 if it is not.
+ */
+int c(char *str, int len, int i)
+{
+if (i >= len)
+return (1);
+if (str[len] == str[i])
+return (checker(str, len - 1, i + 1));
+return (0);
+}
+/**
+ * is_palindrome - target func
+ * @s: parameter
+ * Return: 1 if string is a palindrome, 0 if it is not.
  */
 int is_palindrome(char *s)
 {
-if (!*s)
-return;
-int z = 0,zz=0;
-if(_puts_recursion(s + 1) == _print_rev_recursion(s + 1))
-z++;
-else
-zz++;
-if (z == (z + zz))
-return (1);
-else
-return (0);
+int len = _strlen_recursion(s);
+int i = 0;
+
+return (c(s, len - 1, i));
 }
