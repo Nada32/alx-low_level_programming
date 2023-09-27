39659ecc8fe595e05e3801b861bc00611ba7af79
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-27 11:29:09 -0700

    d

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 
 /**
  * main - Entry point
- * @argc: parameter
- * @argv: parameter
- * Return: Always 0.
+ *
+ * @argc: i/p argument
+ * @argv: i/p argument
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
-	{
+{
+int i, j;
 
-	 int i, z, x = 0, y = 0;
-
-	if (argc < 2)
-	{
-	printf("Error\n");
-	return (1);
-	}
-	for (i = 0; *argv[1] != '\0'; i++)
-	{
-	x = x * 10 + (*argv[1] - 48);
-	}
-	for (i = 0; *argv[2] != '\0'; i++)
-	{
-	y = y * 10 + (*argv[2] - 48);
-	}
-	z = x * y;
-
-	printf("%d\n", z);
-
-	return (0);
-
-	}
+if (argc == 1 || argc == 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+j = 1;
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+printf("%d\n", j);
+}
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,6 @@
 #ifndef main_h
 #define main_h
+#include <stdlib.h>
+
 int _putchar(char c);
 #endif
