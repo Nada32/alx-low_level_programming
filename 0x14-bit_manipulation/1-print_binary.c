2b278324754b4d99577ac9a034cc700d75098105
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-29 12:21:46 -0700

    d

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - Entry point
+ * @b: parameter
+ * Return: Always 0.
+ */
 void print_binary(unsigned long int b)
 {
 if ((b / 2) == 0)
 {
+_putchar(b + '0');
 return;
 }
 print_binary(b / 2);
