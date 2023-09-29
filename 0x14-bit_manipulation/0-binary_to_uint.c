1cf2ac8a04c2205bd95b14ac310a04599e7e99c2
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-29 11:07:12 -0700

    d

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,9 +7,11 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int y = 0;
-int i;
-unsigned int len = 0;
+unsigned int y, z, zz;
+int i, len, j;
+
+y = 0;
+len = 0;
 
 while (b[len] != '\0')
 len++;
@@ -17,11 +19,9 @@ for (i = len - 1; i >= 0; i--)
 {
 if (!(b[i] > ('0' - 1) && b[i]  < ('1' + 1)))
 return (0);
-
-unsigned int z = 1;
-unsigned int zz = 0;
-int j = 0;
-
+z = 1;
+zz = 0;
+j = 0;
 for (j = 0; j < len - i - 1; j++)
 {
 z = 2 * (unsigned int)(b[i] - '0') * z;
